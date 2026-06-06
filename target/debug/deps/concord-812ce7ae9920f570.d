ba119ec89deeeeb02fa1cc1caa8ae57cc3758c84
Joe Yen <jyen.tech@gmail.com>
2026-06-05 21:27:30 -0700

    fix: add .gitignore, remove target/ from tracking

--- target/debug/deps/concord-812ce7ae9920f570.d
+++
@@ -1,7 +0,0 @@
-/root/build/concord/target/debug/deps/concord-812ce7ae9920f570: concord-corpus/src/main.rs
-
-/root/build/concord/target/debug/deps/concord-812ce7ae9920f570.d: concord-corpus/src/main.rs
-
-concord-corpus/src/main.rs:
-
-# env-dep:CARGO_PKG_VERSION=0.1.0
